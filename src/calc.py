75b72dd83ce2edb1f9f58ecad53b61b2dc391a9e
Jose Mendez <jimmymflores@gmail.com>
2022-03-20 11:57:59 -0600

    Modificaciones para cumplir con el PEP8 y docstrings en formato Numpy

--- src/calc.py
+++ src/calc.py
@@ -1,16 +1,65 @@
-def get_fractions(valor : 'string') -> 'float':
-  if("/" in valor):
-    decimal = int (valor[0:1]) / int (valor[2:3])
-    return decimal
-  else:
-    return float(valor)    
+def get_fractions(valor: 'string') -> 'float':
+    """ Convierte fracciones (i.e a/b a,b ∈ ℕ) de string a floats.
 
-def suma(a : 'num_racional',b: 'num_racional') -> 'suma_de_racionales':
-  sumando_a = get_fractions(a)
-  sumando_b = get_fractions(b)
-  return sumando_a + sumando_b
+    Parameters
+    ----------
+    valor: 'string' :
+        Una fracción a/b donde a,b ∈ ℕ.
 
-def multiplica(a : 'num_racional',b: 'num_racional')->'multiplicaion_de_racionales':
-  multiplicando = get_fractions(a)
-  multiplicador = get_fractions(b)
-  return multiplicando * multiplicador
+    Returns
+    -------
+    float
+        El valor de la fracción en decimales.
+
+    """
+    if("/" in valor):
+        decimal = int(valor[0:1]) / int(valor[2:3])
+        return decimal
+    else:
+        return float(valor)
+
+
+def suma(a: 'num_racional', b: 'num_racional') -> 'suma_de_racionales':
+    """Suma dos números racionales a y b (a, b ∈ ℚ).
+
+    Parameters
+    ----------
+    a: 'num_racional' :
+        Número tipo float, o devuelto por la función get_fractions.
+
+    b: 'num_racional' :
+        Número tipo float, o devuelto por la función get_fractions.
+
+    Returns
+    -------
+    float
+        El resultado de sumar a y b.
+
+    """
+    sumando_a = get_fractions(a)
+    sumando_b = get_fractions(b)
+    return sumando_a + sumando_b
+
+
+def multiplica(a: 'num_racional', b: 'num_racional') \
+      -> 'multiplicaion_de_racionales':
+    """Multiplica dos números racionales a y b (a, b ∈ ℚ).
+
+    Parameters
+    ----------
+    a: 'num_racional' :
+        Número tipo float, o devuelto por la función get_fractions.
+
+    b: 'num_racional' :
+        Número tipo float, o devuelto por la función get_fractions.
+
+
+    Returns
+    -------
+    float
+        El resultado de multiplicar a y b.
+
+    """
+    multiplicando = get_fractions(a)
+    multiplicador = get_fractions(b)
+    return multiplicando * multiplicador
